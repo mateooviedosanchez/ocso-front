48e210292465628816e8404531019a72302a26d6
mateooviedosanchez <mateooviedosanchez@gmail.com>
2025-03-22 17:48:41 -0600

    feat: JWT retrieved

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,18 +1,37 @@
+"use client";
+import React from "react";
 import { Button, Input } from "@heroui/react";
+import axios from "axios";
 import Link from "next/link";
+import { API_URL } from "@/constants";
 
 export default function LoginPage() {
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        let authData: any = {};
+        authData.userEmail = formData.get("userEmail");
+        authData.userPassword = formData.get("userPassword");
+        const {data} = await axios.post(`${API_URL}/auth/login`, {
+            ... authData
+        })
+        console.log(data);
+        return;
+    }
     return (
-        <div className="bg-orange-500 px-10 py-2 rounded-md">
-            <p className="text-2xl my-4 text-white text-center">Iniciar Sesion<span></span></p>
+        <form className="bg-orange-500 px-10 py-2 rounded-md" onSubmit={handleSubmit}>
+            <p className="text-2xl my-4 text-white text-center">
+                Iniciar Sesion
+            </p>
             <div className="flex flex-col gap-2 my-4 items-center">
-                <Input label="Email" type="email" isRequired={true} size="sm" />
-                <Input label="Contraseña" type="password" isRequired={true} size="sm" />
+                <Input label="Email" name="userEmail" type="email" isRequired={true} size="sm" />
+                <Input label="Contraseña" name="userPassword" type="password" isRequired={true} size="sm" />
             </div>
             <div className="flex flex-col items-center gap-2">
-                <Button color="primary">Ingresar</Button> 
-                <p className="text-white">No tienes una cuenta? <Link href="/signup" className="text-red-600 underline">Registrarse</Link></p>
+                <Button color="primary" type="submit">Iniciar Sesion</Button> 
+                <p className="text-white">
+                    No tienes una cuenta? <Link href="/signup" className="text-red-600 underline">Registrarse</Link></p>
             </div>
-        </div>
+        </form>
     );
 }
